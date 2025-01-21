c89261f7d26e0a6472ae858e5e56e97230d6344b
layumih <ayumi.cod06@gmail.com>
2025-01-20 23:46:04 -0300

    config do cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://localhost:3000' || 'https://interface-acompanhantes.vercel.app',
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 //  routes
