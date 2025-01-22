6e2b611d1bc4bae9a57a91fed56d912313b227d6
layumih <ayumi.cod06@gmail.com>
2025-01-22 19:47:16 -0300

    implementando crud das acompanhantes. implementando listar e assinar planos - teste

--- backend/routes/plansRoutes.js
+++ backend/routes/plansRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { listPlans, subscribeToPlan } = require('../controllers/planController');
-const { authenticate } = require('../middleware/authMiddleware');
+// const { authenticate } = require('../middleware/authMiddleware');
 
-router.get('/listPlans', authenticate, listPlans);
-router.post('/subscribe', authenticate, subscribeToPlan);
+router.get('/listPlans', listPlans);
+router.post('/subscribe', subscribeToPlan);
 
 module.exports = router;
