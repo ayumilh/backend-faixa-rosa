924524cdbc7f9903c7b76b44a85e1de65ae1f202
layumih <ayumi.cod06@gmail.com>
2025-01-24 10:13:42 -0300

    config authenticate no header

--- backend/routes/plansRoutes.js
+++ backend/routes/plansRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { listPlans, subscribeToPlan, finalizePlan, createUserPlan, listUserPlans, listAvailablePlanTypes, addUserExtras } = require('../controllers/planController');
-const { authenticate } = require('../middleware/authMiddleware');
 
 router.get('/listPlans', listPlans);                      // Listar planos
-router.post('/subscribe', authenticate, subscribeToPlan); // Assinar plano
-router.post('/finalize', authenticate, finalizePlan);     // Finalizar plano
+router.post('/subscribe', subscribeToPlan); // Assinar plano
+router.post('/finalize', finalizePlan);     // Finalizar plano
 
 router.get('/available-types', listAvailablePlanTypes);  // Listar planos básicos disponíveis
 router.post('/create-with-extras', createUserPlan);      // Criar plano básico com extras
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const { authenticate } = require('./middleware/authMiddleware.js');
 
 const app = express();
 
@@ -14,19 +15,23 @@ app.use(
   })
 );
 app.use(express.json());
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+app.use(authenticate);
 
 //  routes
-const authRoutes = require('./routes/authRoutes');
 const verifyToken = require('./routes/utils/utils.js');
 const plansRoutes = require('./routes/plansRoutes.js');
 const companionRoutes = require('./routes/companionRoutes.js');
+
 
 // Rotas
 app.get('/', (req, res) => {
   res.send('API funcionando!');
 });
 
-app.use('/api/auth', authRoutes);
+
 app.use('/api/', verifyToken);
 app.use('/api/plans', plansRoutes);
 app.use('/api/companions', companionRoutes);
