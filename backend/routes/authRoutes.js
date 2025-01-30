b2ac9f75c9d0e4841cb10c3aeb8564cd4e71ab26
layumih <ayumi.cod06@gmail.com>
2025-01-29 21:30:10 -0300

    alterado o verify-cpf para rota publica

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authController.js');
+const cpfController = require('../controllers/utils/cpfController.js');
 
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+router.post('/verify-cpf', cpfController.verificarCPF);
 
 module.exports = router;
