ad401008c8f314ffde38f1236aaa768c05450bba
layumih <ayumi.cod06@gmail.com>
2025-01-30 12:54:49 -0300

    teste do endpoint de financeiro

--- backend/routes/companionRoutes.js
+++ backend/routes/companionRoutes.js
@@ -7,6 +7,7 @@ const {
     updateCompanionServices,
     updateCompanionSchedule,
     updateCompanionLocation,
+    updateAttendedLocations,
     updateCompanionFinanceAndServices,
     listCompanions,
     deleteCompanion
@@ -20,7 +21,8 @@ router.post('/upload-media', uploadCompanionMedia);
 router.put('/update-contact', updateCompanionContact);
 router.put('/update-services', updateCompanionServices);
 router.put('/update-schedule', updateCompanionSchedule);
-router.put('/update-location', updateCompanionLocation);
+router.put('/update-location', updateCompanionLocation);                               // atualizar localização da acompanhante
+router.put("/companions/attended-locations", updateAttendedLocations);  // atualizar locais atendidos pela acompanhante
 router.put('/update-finance', updateCompanionFinanceAndServices);
 router.get('/list', listCompanions);
 router.delete('/delete/:id', deleteCompanion);
