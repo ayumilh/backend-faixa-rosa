397f5ed895caa8f1c1ae556eaf198290540ae91f
layumih <ayumi.cod06@gmail.com>
2025-01-30 13:00:45 -0300

    teste do endpoint de locais atendidos

--- backend/routes/companionRoutes.js
+++ backend/routes/companionRoutes.js
@@ -22,7 +22,7 @@ router.put('/update-contact', updateCompanionContact);
 router.put('/update-services', updateCompanionServices);
 router.put('/update-schedule', updateCompanionSchedule);
 router.put('/update-location', updateCompanionLocation);                               // atualizar localização da acompanhante
-router.put("/companions/attended-locations", updateAttendedLocations);  // atualizar locais atendidos pela acompanhante
+router.put("/attended-locations", updateAttendedLocations);  // atualizar locais atendidos pela acompanhante
 router.put('/update-finance', updateCompanionFinanceAndServices);
 router.get('/list', listCompanions);
 router.delete('/delete/:id', deleteCompanion);
