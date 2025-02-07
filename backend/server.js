a120eeee6981cb088ee1e977cc9a6411d87d245a
layumih <ayumi.cod06@gmail.com>
2025-02-07 10:29:45 -0300

    alterando a ordem dos nmiddalaware

--- backend/server.js
+++ backend/server.js
@@ -19,26 +19,24 @@ app.use(express.urlencoded({ extended: true }));
 
 // Rotas publicas
 const authRoutes = require('./routes/authRoutes');
-const verifyToken = require('./routes/utils/utils.js');
-
 app.use('/api/auth', authRoutes);
-app.use('/api/', verifyToken);
-app.get('/', (req, res) => {
-  res.send('API funcionando!');
-});
 
-app.use(authenticate);
-
-// Rotas privadas
+const verifyToken = require('./routes/utils/utils.js');
 const userRoutes = require('./routes/userRoutes.js');
 const plansRoutes = require('./routes/plansRoutes.js');
 const companionRoutes = require('./routes/companionRoutes.js');
 // const adminRoutes = require('./routes/admin/adminRoutes.js');
 
-// app.use('/api/admin', adminRoutes);
-app.use('/api/users/', userRoutes);
-app.use('/api/plans', plansRoutes);
-app.use('/api/companions', companionRoutes);
+app.get('/', (req, res) => {
+  res.send('API funcionando!');
+});
+app.use('/api/', verifyToken);
+
+// Rotas privadas
+// app.use('/api/admin', authenticate, adminRoutes);
+app.use('/api/users/', authenticate, userRoutes);
+app.use('/api/plans', authenticate, plansRoutes);
+app.use('/api/companions', authenticate, companionRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
