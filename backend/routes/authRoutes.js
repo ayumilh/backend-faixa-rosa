6c7f0207eb229038af64fc7412efb4de7b036f7f
layumih <ayumi.cod06@gmail.com>
2025-02-07 12:08:21 -0300

    Atualizando a função validarCpf em cpfUtils.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController.js');
 const cpfController = require('../controllers/utils/cpfController.js');
-
+const verifyToken = require('../utils/verifyToken.js');
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-router.post('/verify-cpf', cpfController.verificarCPF);
+router.post('/userId', verifyToken.userId);
+router.post('/verify-cpf/', cpfController.verificarCPF);
 
 module.exports = router;
