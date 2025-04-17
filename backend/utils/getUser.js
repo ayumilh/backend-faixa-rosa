9a8eb56d7ac584a5e9ccbf4e2569e97b49d709e2
layumih <ayumi.cod06@gmail.com>
2025-04-17 18:24:35 -0300

    redirecionamento do contratante, ajustando endpooint de getInfo

--- backend/utils/getUser.js
+++ backend/utils/getUser.js
@@ -3,30 +3,60 @@ dotenv.config();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// Get User por Id no Banco
 const getUserIdBd = async (req, res) => {
     try {
+        console.log('getUserIdBd');
+        console.log('req.user', req.user);
+        console.log('req.query', req.query);
         const userid = req.user.id;
+        const tipoPerfil = req.query.tipoPerfil;
 
         if (!userid || isNaN(userid)) {
             return res.status(400).json({ message: 'ID de usuário inválido.' });
         }
 
+        let includeObj = {};
+
+        if (tipoPerfil === "CONTRATANTE") {
+            includeObj.Contractor = true;
+        } else if (tipoPerfil === "ACOMPANHANTE") {
+            includeObj.companion = true;
+        } else {
+            return res.status(400).json({ message: 'Tipo de perfil não especificado ou inválido.' });
+        }
+
         const user = await prisma.user.findUnique({
             where: { id: userid },
-            include: {
-                companion: true,
-            }
+            include: includeObj,
         });
+
+        console.log('user', user);
 
         if (!user) return res.status(404).json({ message: 'Usuário não encontrado.' });
 
-        res.status(200).json(user);
+        let userName = "";
+        if (tipoPerfil === "CONTRATANTE" && user.Contractor?.userName) {
+            userName = user.Contractor.userName;
+            delete user.Contractor.userName;
+        } else if (tipoPerfil === "ACOMPANHANTE" && user.companion?.userName) {
+            userName = user.companion.userName;
+            delete user.companion.userName;
+        }
+
+        const cleanUser = {
+            ...user,
+            userName,
+        };
+
+        // Retorna também o tipo
+        res.status(200).json(cleanUser);
+
     } catch (error) {
         console.error('Erro:', error);
         res.status(500).json({ message: 'Erro ao recuperar o usuário do banco de dados.' });
     }
 };
+
 
 
 module.exports = {
